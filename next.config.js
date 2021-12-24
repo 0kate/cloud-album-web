eb55ad36c1cce1454a714e25b901f49296aa9554
0K4T3 <o.keito317@icloud.com>
2021-12-24 11:13:07 +0900

    add: pwa configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+const withPWA = require('next-pwa')
+
 /** @type {import('next').NextConfig} */
 module.exports = {
-  basePath: '/album',
   reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
 }
