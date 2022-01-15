bf4f24085a830aed1dbd306218030c4b359d80a2
0K4T3 <o.keito317@icloud.com>
2022-01-15 18:40:56 +0900

    add: confirmation dialog component

---
+++ components/ConfirmationDialog.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+import {
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Typography,
+} from '@mui/material';
+import styles from './ConfirmationDialog.module.css';
+
+interface Props {
+  message: string;
+  open: boolean;
+  onClose: () => void;
+  onClickCancel: () => void;
+  onClickOk: () => void;
+  inProcessing: boolean;
+};
+
+const ConfirmationDialog: FC<Props> = ({ message, open, onClose, onClickCancel, onClickOk, inProcessing }: Props) => {
+  return (
+    <Dialog open={open} onClose={onClose} fullWidth>
+      <DialogContent>
+	<Typography className={styles.message} variant="subtitle">{message}</Typography>
+      </DialogContent>
+      <DialogActions>
+	<Button variant="outlined" onClick={onClickCancel} disabled={inProcessing}>
+	  CANCEL
+	</Button>
+	<Button variant="contained" onClick={onClickOk} disabled={inProcessing}>
+	  {inProcessing ? <CircularProgress color="grey" size={25} /> : "OK"}
+	</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ConfirmationDialog;
